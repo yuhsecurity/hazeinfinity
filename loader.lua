1bf7402930412df71c1a2362ff106030d4c7ddbc
Yuh <75444356+yuhsecurity@users.noreply.github.com>
2021-01-13 11:05:02 -0600

    Initial Commit
    Added Loader & Scripts

--- loader.lua
+++ loader.lua
@@ -1,6 +1,8 @@
--- Haze Hub Loader by yuhsecurity
--- Scripts Not Coded by Me
--- Line Break
+-- +++++++++++++++++++++++++++++++++++++++
+-- + Haze Infinity Loader by yuhsecurity +
+-- + Scripts Not Coded by Me             +
+-- +++++++++++++++++++++++++++++++++++++++
+-- If Unnamed ESP isn't working enable show team
 if game.PlaceId == 292439477 then -- Phantom Forces
 loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/cumhaxx/main/Phantom%20Forces"))()
 -- Line Break
@@ -13,6 +15,8 @@ loadstring(game:HttpGet(""))()
 elseif game.PlaceId == 286090429 then -- Arsenal
 loadstring(game:HttpGet(""))()
 -- Line Break
+elseif game.PlaceId == 394206297 then -- Kinetic Code
+loadstring(game:HttpGet(""))()
 else
 game.Players.LocalPlayer:Kick("Game Not Supported by Haze Hub | discord.gg/QvgkHubJT7") -- Trigger GNS Error
 end
