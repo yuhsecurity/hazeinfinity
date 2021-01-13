61fcd86f7f24202c7ee3bc18cef5e72e2a66ad49
Yuh <75444356+yuhsecurity@users.noreply.github.com>
2021-01-13 12:05:52 -0600

    Finalized Loadstrings
    Changed PF & BB Loadstrings, Added Counterblox

--- loader.lua
+++ loader.lua
@@ -5,10 +5,10 @@
 -- If Unnamed ESP isn't working enable show team
 --
 if game.PlaceId == 292439477 then -- Phantom Forces
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/cumhaxx/main/Phantom%20Forces"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/phantomforces.lua"))()
 --
 elseif game.PlaceId == 3233893879 then -- Bad Business
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/cumhaxx/main/Bad%20Business"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/badbusiness.lua"))()
 --
 elseif game.PlaceId == 2377868063 then -- Strucid
 loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/esp.lua"))()
@@ -18,6 +18,9 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfin
 --
 elseif game.PlaceId == 394206297 then -- Kinetic Code
 loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/esp.lua"))()
+--
+elseif game.PlaceId == 301549746 then -- Counter Blox
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/counterblox.lua"))()
 else
 game.Players.LocalPlayer:Kick("Game Not Supported by Haze Hub | discord.gg/QvgkHubJT7") -- Trigger GNS Error
 end
