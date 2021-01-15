53eb2cb5a285feeba017099846a11d7317761d94
Yuh <75444356+yuhsecurity@users.noreply.github.com>
2021-01-14 21:14:50 -0600

    Big pp update
    check #updatelog in the discord

--- loader.lua
+++ loader.lua
@@ -7,31 +7,75 @@
 --
 if game.PlaceId == 292439477 then -- Phantom Forces
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/phantomforces.lua"))()
+    local h = Instance.new("Message")
+    h.Parent = Workspace 
+    h.Text = "Haze Infinity | Phantom Forces Detected, Launching EHUB"
+    wait(5)
+    h:Remove()
     --
     elseif game.PlaceId == 3233893879 then -- Bad Business
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/badbusiness.lua"))()
+    local h = Instance.new("Message")
+    h.Parent = Workspace 
+    h.Text = "Haze Infinity | Bad Business Detected, Launching CumHaxx"
+    wait(5)
+    h:Remove()
     --
     elseif game.PlaceId == 2377868063 then -- Strucid
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/strucid.lua"))()
+    local h = Instance.new("Message")
+    h.Parent = Workspace 
+    h.Text = "Haze Infinity | Strucid Detected, Launching Dark Hub"
+    wait(5)
+    h:Remove()
     --
     elseif game.PlaceId == 2633045061 then -- Strucid BR Queue
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/strucid.lua"))()
+    local h = Instance.new("Message")
+    h.Parent = Workspace 
+    h.Text = "Haze Infinity | ERROR: Don't execute until in-game"
+    wait(5)
+    h:Remove()
     --
     elseif game.PlaceId == 3606833500 then -- Strucid Zone Wars
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/strucid.lua"))()
+    local h = Instance.new("Message")
+    h.Parent = Workspace 
+    h.Text = "Haze Infinity | Strucid Detected, Launching DarkHub"
+    wait(5)
+    h:Remove()
     --
     elseif game.PlaceId == 2674164583 then -- Strucid Battle Royale
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/strucid.lua"))()
+    local h = Instance.new("Message")
+    h.Parent = Workspace 
+    h.Text = "Haze Infinity | Strucid Detected, Launching DarkHub"
+    wait(5)
+    h:Remove()
     --
     elseif game.PlaceId == 286090429 then -- Arsenal
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/arsenal.lua"))()
+    local h = Instance.new("Message")
+    h.Parent = Workspace 
+    h.Text = "Haze Infinity | Arsenal Detected, Launching OP Silent Aim"
+    wait(5)
+    h:Remove()
     --
     elseif game.PlaceId == 394206297 then -- Kinetic Code
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/esp.lua"))()
+    local h = Instance.new("Message")
+    h.Parent = Workspace 
+    h.Text = "Haze Infinity | Kinetic Code Detected, Launching Unnamed ESP"
+    wait(5)
+    h:Remove()
     --
     elseif game.PlaceId == 301549746 then -- Counter Blox
     loadstring(game:HttpGet("https://raw.githubusercontent.com/yuhsecurity/hazeinfinity/main/scripts/counterblox.lua"))()
+    local h = Instance.new("Message")
+    h.Parent = Workspace 
+    h.Text = "Haze Infinity | Counter Blox Detected, Launching Vex Hub"
+    wait(5)
+    h:Remove()
     else
-    game.Players.LocalPlayer:Kick("Game Not Supported by Haze Hub | discord.gg/QvgkHubJT7") -- Trigger GNS Error
+    game.Players.LocalPlayer:Kick("Game Not Supported by Haze Infinity | discord.gg/QvgkHubJT7") -- Trigger GNS Error
     end
     
