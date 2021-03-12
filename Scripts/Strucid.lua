36159c0d82f142472b9c63e08d17de6f9fa6fe3c
Yuh <75444356+yuhsecurity@users.noreply.github.com>
2021-03-12 10:14:59 -0600

    Update Strucid.lua

--- Scripts/Strucid.lua
+++ Scripts/Strucid.lua
@@ -1,6 +1,6 @@
 game.StarterGui:SetCore("SendNotification", {
     Title = "Haze Infinity"; -- Required. Has to be a string!
-    Text = "Strucid Script Disabled"; -- Required. Has to be a string!
+    Text = "Strucid Script Disabled, We are working on the issue"; -- Required. Has to be a string!
     Icon = ""; -- Optional, defaults to "" (no icon)
     Duration = 5; -- Optional, defaults to 5 seconds
     Button1 = "Close"; -- Optional, makes a button appear with the given text that, when clicked, fires the Callback if it's given
